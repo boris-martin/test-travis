47f74d57ddb8d32fcc406b74b6152e72320151c2
Boris Martin <boris.martin.be@gmail.com>
2021-07-25 12:32:26 +0200

    Added Eigen dep

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <eigen3/Eigen/Dense>
 
 int main() {
     auto f = []() {
+        Eigen::MatrixXd m(2, 2);
+        std::cout << m << std::endl;
         std::cout << "Hello from lambda !" << std::endl;
     };
 
